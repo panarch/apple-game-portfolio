3a519b9a9a961aa031658f73eeef214b9cb248e1
Taehoon Moon <taehoon.moon@outlook.com>
2025-05-10 22:25:37 +0900

    Add Hello World heading to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1>Hello World</h1>
+      </body>
     </html>
   );
 }
