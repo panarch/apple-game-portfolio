74e35b1a891e613ce196d87ae02956c62246c239
Taehoon Moon <taehoon.moon@outlook.com>
2025-05-10 23:05:58 +0900

    Update Header component design

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,9 +4,12 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">About</Link>
-      <Link href="/play">Play</Link>
-      <Link href="/ranking">Ranking</Link>
+      <h1>Apple Game</h1>
+      <nav>
+        <Link href="/">About</Link>
+        <Link href="/play">Play</Link>
+        <Link href="/ranking">Ranking</Link>
+      </nav>
     </header>
   );
 }
