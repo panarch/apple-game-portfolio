df9440709a9e4d11bfdf996627cb9096f3ae2dcc
Taehoon Moon <taehoon.moon@outlook.com>
2025-05-11 00:00:42 +0900

    Add Galindo Google font and apply as global font-family

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,17 @@
+import { Galindo } from "next/font/google";
 import Header from "./components/Header";
 import "./globals.css";
+
+const galindo = Galindo({
+  variable: "--font-galindo",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={galindo.variable}>
         <Header />
         {children}
       </body>
