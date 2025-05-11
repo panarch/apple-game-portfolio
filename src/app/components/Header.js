b8c78f46c8ea693759aeeb3dd03fdb41f3fea32d
Taehoon Moon <taehoon.moon@outlook.com>
2025-05-11 17:01:04 +0900

    Make header title a link and update header styles

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,7 +4,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Apple Game</h1>
+      <Link href="/" className={styles.title}>
+        Apple Game
+      </Link>
+
       <nav>
         <Link href="/">About</Link>
         <Link href="/play">Play</Link>
